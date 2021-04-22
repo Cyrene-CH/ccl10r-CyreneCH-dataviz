33e5eff67f42ea051c1a6a40dd91c3c6fa703e89
CYRENE <xc2234@nyu.edu>
2021-04-22 20:40:27 +0800

    final

--- sketch.js
+++ sketch.js
@@ -6,21 +6,68 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  //angleMode(DEGREES)
 }
 
 function draw() {
   background(0);
   fill(0);
   for (let p of data.bodypoints){
-    let x = p.x;
-    let y = p.y;
-    //circle(x, y, 2);
-    textSize(5);
-    //text(p.part, x, y);
+    let c=p.x
+    let d=p.y
+    let x0 = data.bodypoints[9].x;
+    let y0= data.bodypoints[9].y;
+    let x1 = data.bodypoints[10].x
+    let y1 = data.bodypoints[10].y
+    let x3=data.bodypoints[7].x
+    let y3=data.bodypoints[7].y
+    let x4=data.bodypoints[8].x
+    let y4=data.bodypoints[8].y
+    
+
+    let r=dist(x1, y1, x4, y4)
+    let angle = atan2(y1 - y4, x1 - x4);
+    angle += 0.9 * sin(frameCount / 15);
+	x1 = x4 + r * cos(angle);
+	y1 = y4 + r * sin(angle);
+  line(x1, y1, x4, y4)
+
+    circle(x1,y1,2)
+  
+  
+    push()
+    fill(240, 213, 192)
+    noStroke()
+    let ag=0
+ag=ag+sin(frameCount /15)
+let i=x4+12*cos(ag)
+let j=y4+12*sin(ag)
+    circle(i-10,j-1,20)
+    pop()
+
+
+    let r2=dist(x0, y0, x3, y3)
+    let angle2 = atan2(y0 - y3, x0- x3);
+    angle2 += 1.1 * sin(frameCount / 10);
+	x0 = x3 + r2 * cos(angle2);
+	y0 = y3+ r2 * sin(angle2);
+  line(x0, y0, x3, y3)    
+  circle(x0, y0, 2);
+    push()
+    fill(240, 213, 192)
+    noStroke()
+    let ag2=0
+ag2=ag2+sin(frameCount /10)
+let g=x3+12*cos(ag2)
+let f=y3+12*sin(ag2)
+    circle(g-10,f,20)
+    pop()
+    // textSize(5); 
+    // text(p.part, c,d);
   }
   fill(255)
   beginShape()
-  vertex(data.bodypoints[5].x,data.bodypoints[5].y)
+  vertex(data.bodypoints[6].x,data.bodypoints[6].y);
   vertex(data.bodypoints[11].x,data.bodypoints[11].y);
   vertex(data.bodypoints[13].x,data.bodypoints[13].y);
   vertex(data.bodypoints[15].x,data.bodypoints[15].y);
@@ -28,31 +75,25 @@ function draw() {
   vertex(data.bodypoints[14].x,data.bodypoints[14].y);
   vertex(data.bodypoints[12].x,data.bodypoints[12].y);
   vertex(data.bodypoints[6].x,data.bodypoints[6].y);
+
+  endShape(CLOSE)
+
+  beginShape()
+  vertex(data.bodypoints[5].x,data.bodypoints[5].y)
+  vertex(data.bodypoints[11].x,data.bodypoints[11].y)
+  vertex(data.bodypoints[12].x,data.bodypoints[12].y)
   vertex(data.bodypoints[5].x,data.bodypoints[5].y)
   endShape(CLOSE)
 
 strokeWeight(2)
 stroke(255)
-line(data.bodypoints[8].x,data.bodypoints[8].y,data.bodypoints[10].x,data.bodypoints[10].y)
-line(data.bodypoints[7].x,data.bodypoints[7].y,data.bodypoints[9].x,data.bodypoints[9].y)
 push()
 noStroke()
-// beginShape()
-// vertex(data.bodypoints[17].x,data.bodypoints[17].y)
-// vertex(data.bodypoints[1].x,data.bodypoints[1].y);
-// vertex(data.bodypoints[3].x,data.bodypoints[3].y);
-// vertex(data.bodypoints[0].x,data.bodypoints[0].y);
-// vertex(data.bodypoints[4].x,data.bodypoints[4].y);
-// vertex(data.bodypoints[2].x,data.bodypoints[2].y);
-// vertex(data.bodypoints[17].x,data.bodypoints[17].y)
-// endShape(CLOSE)
+
 fill(240, 213, 192)
-ellipse(305,95,30)
-ellipse(238,224,20)
-ellipse(428,157,20)
-fill(255)
-ellipse(203,262,5)
-ellipse(483,179,5)
+ellipse(305,95,40)
+fill("red")
+ellipse(299,102,9)
 pop()
 
 }
